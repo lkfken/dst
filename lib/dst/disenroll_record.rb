042dd6e40056c0b29c8ca16fdbf908c3da77f6f2
Kenneth Leung <kenneth@leungs.us>
2016-05-31 13:02:19 -0700

    add DisenrollRecord.exclude_cancel_records and DisenrollRecord.filter_by_date

--- lib/dst/disenroll_record.rb
+++ lib/dst/disenroll_record.rb
@@ -3,5 +3,15 @@ module DST
     set_dataset :disenroll_base_view
     many_to_one :member
 
+    dataset_module do
+      def exclude_cancel_records
+        exclude(:begin_cov => :disenr_dt)
+      end
+
+      # this one include cancel records.  Should considering using #eligibility_record_on instead
+      def filter_by_date(date)
+        filter { |record| (record.begin_cov <= date) & (record.disenr_dt >= date) }
+      end
+    end
   end
 end
