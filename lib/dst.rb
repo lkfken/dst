0ff8da82d7c4f51ea6f981225c0eb8c0dc8c5081
Kenneth Leung <kenneth@leungs.us>
2016-05-31 12:44:04 -0700

    added tables.

--- lib/dst.rb
+++ lib/dst.rb
@@ -1,9 +1,12 @@
 require 'bundler'
-Bundler.setup
+Bundler.require(:default)
 require 'dotenv'
 Dotenv.load!
 
 require 'sequel_connect'
+require_relative 'dst/disenroll_record'
+require_relative 'dst/group'
+require_relative 'dst/lob240_benefit_plan_history'
 require_relative 'dst/member'
 require_relative 'dst/physician'
 
