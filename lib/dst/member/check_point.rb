c234640ad714e85ad2de172e564734729e5000bc
Kenneth Leung <kenneth@leungs.us>
2016-06-02 11:36:05 -0700

    MemberClass::CheckPoint module holds all member's boolean methods.

---
+++ lib/dst/member/check_point.rb
@@ -0,0 +1,17 @@
+module MemberClass
+  module CheckPoint
+    def is_active?
+      !is_disenrolled?
+    end
+
+    def is_disenrolled?
+      disenr == 'D' || group_num == ''
+    end
+
+    def is_eligible_on?(date)
+      return true if is_active? && beg_cov <= date
+      records = disenroll_records_dataset.exclude_cancel_records.on(date)
+      !records.count.zero?
+    end
+  end
+end
