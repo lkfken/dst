8cd8db6e028b13c5cb0089b0f52f8bb77808da23
Kenneth Leung <kenneth@leungs.us>
2016-06-02 11:39:04 -0700

    refactoring

--- lib/dst/group.rb
+++ lib/dst/group.rb
@@ -1,10 +1,16 @@
 module DST
   class Group < ::Sequel::Model(::SequelConnect::DB)
+    include DST
     set_dataset :cchp_groups_base
-    one_to_many :members, :key => :group_num, :primary_key => :group_id
+    set_primary_key :group_id
+
+    one_to_many :members, :key => :group_num
+    one_to_many :disenroll_records, :key => :c_grp
+
+    def_column_alias :id, :group_id
 
     def is_exchange?
-      %w[140 240].include?(lob)
+      EXCHANGE_LOBS.include?(lob)
     end
   end
 end
