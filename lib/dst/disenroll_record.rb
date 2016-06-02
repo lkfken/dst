c3dad8f42002a2c50f80b8fad0ef854080706e70
Kenneth Leung <kenneth@leungs.us>
2016-06-02 11:36:28 -0700

    refactoring

--- lib/dst/disenroll_record.rb
+++ lib/dst/disenroll_record.rb
@@ -2,6 +2,9 @@ module DST
   class DisenrollRecord < ::Sequel::Model(::SequelConnect::DB)
     set_dataset :disenroll_base_view
     many_to_one :member
+    many_to_one :group
+
+    def_column_alias :group_id, :c_grp
 
     dataset_module do
       def exclude_cancel_records
@@ -9,7 +12,7 @@ module DST
       end
 
       # this one include cancel records.  Should considering using #eligibility_record_on instead
-      def filter_by_date(date)
+      def on(date)
         filter { |record| (record.begin_cov <= date) & (record.disenr_dt >= date) }
       end
     end
