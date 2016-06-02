4a2329667e1a942ed5ec9b1d87be64c99d67462a
Kenneth Leung <kenneth@leungs.us>
2016-06-02 11:41:32 -0700

    refactoring

--- lib/dst/member.rb
+++ lib/dst/member.rb
@@ -1,41 +1,49 @@
+require_relative 'member/check_point'
+require_relative 'member/group'
+
 module DST
   class Member < ::Sequel::Model(::SequelConnect::DB)
+    include DST
+    include MemberClass::CheckPoint
+    include MemberClass::Group
     set_dataset :members_base_view
-    set_primary_key [:mem_no]
+    set_primary_key :mem_no
 
-    many_to_one :group
     many_to_one :pcp, :class => :'DST::Physician', :key => [:physician, :mem_lob], :primary_key => [:provider_id, :lob]
 
     one_to_many :lob_240_benefit_plan_history, :key => :mem_id, :order => :eff_dt
     one_to_many :disenroll_records, :key => :mem_id
 
-    def is_active?
-      !is_disenrolled?
+    def_column_alias :id, :mem_no
+
+    ########## Typecasting ##########
+
+    def beg_cov
+      super.to_date
     end
 
-    def is_disenrolled?
-      disenr == 'D' || group_num == ''
+    def birth
+      super.to_date
     end
 
-    def group_id(params={})
-      date     = params.fetch(:on, beg_cov)
-      return group_num if is_active? && date >= beg_cov
-      disenrolled_group_id_on(date)
+    def mem_reg_eff
+      super.to_date
     end
 
-    def disenrolled_group_id_on(date)
-      return disenroll_records.first.group_id if date.year == 1899
-      record = disenroll_records_dataset.exclude_cancel_records.filter_by_date(date).first
-      record.nil? ? nil : record.group_id
+    #################################
+
+    def disenroll_records_on(date, include_cancel)
+      ds = disenroll_records_dataset.on(date)
+      ds = ds.exclude_cancel_records unless include_cancel
+      ds.all
     end
 
     def benefit_plan(params={})
-      date     = params.fetch(:on, beg_cov)
-      group_id = group_id(on: date)
-      if group_id.nil?
-        nil
+      date  = params.fetch(:on)
+      group = group(params)
+      if group.nil?
+        NullRecord.new
       else
-        group = DST::Group.where(:group_id => group_id).first
         if group.is_exchange?
           lob_240_benefit_plan_history_dataset.where('eff_dt <= ?', date).reverse(:eff_dt).first
         else
@@ -44,10 +52,19 @@ module DST
       end
     end
 
+    def age(params={})
+      now = params.fetch(:on, Time.now.utc.to_date)
+      now.year - birth.year - ((now.month > birth.month || (now.month == birth.month && now.day >= birth.day)) ? 0 : 1)
+    end
+
     dataset_module do
+      subset :commercial, :mem_lob => COMMERCIAL_LOBS
+      subset :exchange, :mem_lob => EXCHANGE_LOBS
+
       def active
         where(Sequel.&(Sequel.~(:disenr => 'D'), Sequel.~(:mem_lob => '')))
       end
     end
   end
 end
+
