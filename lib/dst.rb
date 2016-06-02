96a0a2b0ccc412e1cfe09f982b9ab8b3e3033214
Kenneth Leung <kenneth@leungs.us>
2016-06-02 11:42:50 -0700

    refactoring

--- lib/dst.rb
+++ lib/dst.rb
@@ -3,6 +3,8 @@ Dotenv.load!
 require 'bundler'
 Bundler.require
 
+require_relative 'dst/null_record'
+
 require_relative 'dst/disenroll_record'
 require_relative 'dst/group'
 require_relative 'dst/lob240_benefit_plan_history'
@@ -10,5 +12,8 @@ require_relative 'dst/member'
 require_relative 'dst/physician'
 
 module DST
+  COMMERCIAL_LOBS = %w[100 110 130 200 220 230]
+  EXCHANGE_LOBS   = %w[140 240]
+  MEDICARE_LOBS   = %w[250 253 256]
 end
 
