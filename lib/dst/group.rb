859f6ca59b8f961bff54fc09630235b55f8a3379
Kenneth Leung <kenneth@leungs.us>
2016-06-02 15:40:13 -0700

    added benefit plan association

--- lib/dst/group.rb
+++ lib/dst/group.rb
@@ -3,13 +3,18 @@ module DST
     set_dataset :cchp_groups_base
     set_primary_key :group_id
 
-    one_to_many :members, :key => :group_num
+    one_to_many :benefit_plans, :key => :group_id, :primary_key => :group_id
     one_to_many :disenroll_records, :key => :c_grp
+    one_to_many :members, :key => :group_num
 
     def_column_alias :id, :group_id
 
     def is_exchange?
       DST.exchange_lobs.include?(lob)
+    end
+
+    def benefit_plan(params={})
+      benefit_plans_dataset.record!(params)
     end
   end
 end
