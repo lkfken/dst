d3a0610602881d00bc1c12ff40b9737e09f5aa14
Kenneth Leung <kenneth@leungs.us>
2016-06-02 12:42:34 -0700

    Initial commit.

---
+++ lib/dst/member/typecasting.rb
@@ -0,0 +1,15 @@
+module DST
+  class Member
+    def beg_cov
+      super.to_date
+    end
+
+    def birth
+      super.to_date
+    end
+
+    def mem_reg_eff
+      super.to_date
+    end
+  end
+end
