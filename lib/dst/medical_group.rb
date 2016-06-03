d4e9027701b84b21c5cde8e6ab45a76ab8294783
Kenneth Leung <kenneth@leungs.us>
2016-06-03 15:38:53 -0700

    Initial commit.

---
+++ lib/dst/medical_group.rb
@@ -0,0 +1,49 @@
+module DST
+  class MedicalGroup < ::Sequel::Model(::SequelConnect::DB)
+    VALID_PCP_CAPACITY_CODE = %w[PPCP FPCP]
+
+    set_dataset :prov_region_base_view
+    set_primary_key [:provider_id, :lob, :prov_reg_eff_dt, :prov_capacity]
+
+    ########## associations ##########
+
+    many_to_one :physician, :class => :'MhcModels::Physician',
+                :key               => [:provider, :lob], :primary_key => [:provider_id, :lob]
+
+    ##################################
+
+    dataset_module do
+
+      def span(params={})
+        end_date = params.fetch(:end_date, Date.civil(2999, 12, 31))
+        ds       = where('prov_reg_eff_dt <= ?', end_date)
+        return ds if ds.empty?
+
+        start_date    = params.fetch(:start_date, Date.civil(1900, 1, 1))
+        ds_start_date = ds.exclude('prov_reg_eff_dt > ?', start_date).max(:prov_reg_eff_dt)
+        ds_start_date.nil? ? ds : ds.where('prov_reg_eff_dt >= ?', ds_start_date)
+      end
+
+      def active
+        where(:active_status => '1')
+      end
+
+      def pcp
+        where(:prov_capacity => VALID_PCP_CAPACITY_CODE)
+      end
+
+      def eligible(params={})
+        date = params.fetch(:on)
+        span(:end_date => date).active
+      end
+
+      def hpmg
+        where(:region => 'HPMG')
+      end
+
+      def regions!
+        select_group(:region).map(:region)
+      end
+    end
+  end
+end
