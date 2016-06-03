e7af9523eb46220bf2f8e149044e86cd5f0f3a2b
Kenneth Leung <kenneth@leungs.us>
2016-06-03 15:46:44 -0700

    Bug fix

--- lib/dst/physician.rb
+++ lib/dst/physician.rb
@@ -1,7 +1,11 @@
+require_relative 'physician/medical_group'
 module DST
   class Physician < ::Sequel::Model(::SequelConnect::DB)
-    set_dataset :physician_base_view
-    one_to_many :member, :key => [:provider_id, :lob], :primary_key => [:physician, :mem_lob]
 
+    set_dataset :physician_base_view
+    set_primary_key [:provider_id, :lob]
+
+    one_to_many :member, :key => [:provider_id, :lob], :primary_key => [:physician, :mem_lob]
+    one_to_many :medical_groups, :class => :'DST::MedicalGroup', :key => [:provider, :lob]
   end
 end
