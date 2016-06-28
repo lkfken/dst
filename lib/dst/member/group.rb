550c3e82fbac82332f1d6bde33abf35a83c78ab3
Kenneth Leung <kenneth@leungs.us>
2016-06-28 16:44:04 -0700

    raise an exception if the member is eligible on a given date and found multiple group ids.

--- lib/dst/member/group.rb
+++ lib/dst/member/group.rb
@@ -1,6 +1,24 @@
 module DST
   module MemberInstanceMethods
     module Group
+      class MultipleGroupIdOnDateError < StandardError
+        def initialize(params={})
+          date      = params.fetch(:date)
+          member_id = params.fetch(:member_id)
+          group_ids = params.fetch(:group_ids)
+          msg       = "On #{date}, multiple group ID #{group_ids} found for member #{member_id}"
+          super(msg)
+        end
+      end
+      class NoGroupIdOnDateError < StandardError
+        def initialize(params={})
+          date      = params.fetch(:date)
+          member_id = params.fetch(:member_id)
+          msg       = "On #{date}, no group ID found for member #{member_id}"
+          super(msg)
+        end
+      end
+
       def group_ids(params={})
         date           = params.fetch(:on)
         include_cancel = params.fetch(:include_cancel, true)
@@ -12,7 +30,10 @@ module DST
 
       def group_id(params={})
         date = params.fetch(:on)
-        group_ids(:on => date, :include_cancel => false).first
+        return nil unless is_eligible_on?(date)
+        ids  = group_ids(:on => date, :include_cancel => false)
+        raise(MultipleGroupIdOnDateError, :date => date, :member_id => id, :group_ids => ids) if ids.size > 1
+        ids.first
       end
 
       def group(params={})
